fc8224b1ed4e7d0c6c0b709bbfca89a5c05f7a95
Gordon Hollingworth <gordon@raspberrypi.com>
2024-10-25 15:31:21 +0100

    Create depends.sh
    Not really sure why we have depends but not this, which is of course what you actually need!  Done this though the web interface so it needs adding +x to the script...  Otherwise would have pushed

---
+++ depends.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt install quilt parted coreutils qemu-user-static debootstrap zerofree zip dosfstools libcap2-bin libarchive-tools grep rsync xz-utils curl xxd file git kmod bc gpg pigz arch-test genimage mtools mmdebstrap bdebstrap podman
