d6a871330068f5f38acb234d4b73b16874052db2
antonio-molteni-spi <antonio.molteni@digilock.com>
2025-11-25 20:01:55 +0000

    increase root size

--- digilock/onprem-demo-slim/image/compact/pre-image.sh
+++ digilock/onprem-demo-slim/image/compact/pre-image.sh
@@ -7,7 +7,7 @@ genimg_in=$2
 
 FW_SIZE=100%
 # 100% is minimal storage leaves no room for loading docker containers. 200% leaves overhead.
-ROOT_SIZE=200%
+ROOT_SIZE=300%
 
 cat genimage.cfg.in | sed \
    -e "s|<IMAGE_DIR>|$IGconf_image_outputdir|g" \
