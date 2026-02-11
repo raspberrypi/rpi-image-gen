d19b1a44d4f6f0f5747e495b245d6eee03b28437
Matthew Lear <matthew.lear@raspberrypi.com>
2026-02-11 08:12:41 +0000

    bootstrap: dynamic pkg build selection from triggers
    Layers can now set env vars via triggers. lib/tools.sh uses a small
    registry to map those vars to packages in order to build only the
    selected set into the mini-sysroot. This keeps config and bootstrap
    loosely coupled and easy to extend.
    
    Trigger off image provider to select genimage for build/install.

--- lib/tools.sh
+++ lib/tools.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
+# Dynamic package build selection. Typically, X-Env Triggers set defined
+# variables which map to the package registry below. These compoments are
+# built and installed as prerequisites in a reusable mini-sysroot.
+# Can scale as needed, eg move away from bash array to file. Main thing
+# is to retain functionality: vars -> registry -> selection -> action
+readonly -a IG_TOOLS_REGISTRY=(
+   "IG_ENABLE_HOST_GENIMAGE|y|genimage"
+)
+
+collect_build_deps() {
+   local envfile="${1:?missing env file}"
+   local packages=(bdebstrap) # mandatory
+   local var value pkg
+
+   for entry in "${IG_TOOLS_REGISTRY[@]}"; do
+      IFS='|' read -r var value pkg <<< "$entry"
+      if value_read=$(get_var "$var" "$envfile") && [[ "$value_read" == "$value" ]]; then
+         packages+=("$pkg")
+      fi
+   done
+   echo "${packages[*]}"
+}
+
+
 bootstrap_build_tools() {
    : "${ctx[FINALENV]?missing ctx[FINALENV]}"
    : "${IGconf_sys_workroot?missing IGconf_sys_workroot}"
    : "${DEB_BUILD_GNU_TYPE?missing DEB_BUILD_GNU_TYPE}"
 
-   local tools=(bdebstrap)
-   if value=$(get_var IGconf_image_provider "${ctx[FINALENV]}") \
-      && [[ $value == genimage ]]; then
-      tools+=(genimage)
-   fi
-
+   local tools=( $(collect_build_deps "${ctx[FINALENV]}") )
    local destdir="${IGconf_sys_workroot}/${DEB_BUILD_GNU_TYPE}"
    local prefix=/usr
 
