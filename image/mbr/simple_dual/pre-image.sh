50b83f9e195323bf1fdf38f8379ec145ace623ba
Matthew Lear <matthew.lear@raspberrypi.com>
2026-03-02 16:26:07 +0000

    image-rpios: mkfs arg tuning and defaults
    Simplify genimage template to build all vfat args in pre-image, but
    reference the defaults from fs-base, therefore allowing a user to
    override as they wish.
    
    ext4 filesystems default to a block size equivalent to the page size of
    the target kernel. btrfs filesystems still use empty defaults simply
    because mkfs.btrfs can grow a filesystem as-needed, sometimes resulting
    in it being greater than the size of the file created for it to fill.

--- image/mbr/simple_dual/pre-image.sh
+++ image/mbr/simple_dual/pre-image.sh
@@ -22,6 +22,7 @@ done
 
 MKE2FS_ARGS_STR="-U $ROOT_UUID ${IGconf_fs_ext4_mkfs_args:-}"
 BTRFS_ARGS_STR="-U $ROOT_UUID ${IGconf_fs_btrfs_mkfs_args:-}"
+VFAT_ARGS_STR="-S $IGconf_device_sector_size -i $BOOT_LABEL ${IGconf_fs_vfat_mkfs_args:-}"
 
 # Write genimage template
 cat genimage.cfg.in.$IGconf_image_rootfs_type | sed \
@@ -30,12 +31,11 @@ cat genimage.cfg.in.$IGconf_image_rootfs_type | sed \
    -e "s|<IMAGE_SUFFIX>|$IGconf_image_suffix|g" \
    -e "s|<FW_SIZE>|$IGconf_image_boot_part_size|g" \
    -e "s|<ROOT_SIZE>|$IGconf_image_root_part_size|g" \
-   -e "s|<SECTOR_SIZE>|$IGconf_device_sector_size|g" \
    -e "s|<SETUP>|'$(readlink -ef setup.sh)'|g" \
    -e "s|<MKE2FS_CONF>|'$(readlink -ef mke2fs.conf)'|g" \
    -e "s|<MKE2FS_EXTRAARGS>|$MKE2FS_ARGS_STR|g" \
    -e "s|<BTRFS_EXTRAARGS>|$BTRFS_ARGS_STR|g" \
-   -e "s|<BOOT_LABEL>|$BOOT_LABEL|g" \
+   -e "s|<VFAT_EXTRAARGS>|$VFAT_ARGS_STR|g" \
    -e "s|<BOOT_UUID>|$BOOT_UUID|g" \
    -e "s|<ROOT_UUID>|$ROOT_UUID|g" \
    > ${genimg_in}/genimage.cfg
